a2914d47dc8fe676c4bbca9422c82a4045b69723
Nitish Pal <nitishpal2013@gmail.com>
2023-08-12 16:13:50 +0530

    Project Accepted!

--- server.js
+++ server.js
@@ -2,33 +2,27 @@ const express = require('express')
 const app = express()
 const port = 80
 
+function dateConverter(data){
+    let date;
+    if(data){
+        if(+data > 0) date = new Date(+data);
+        else date = new Date(data);
+    }
+    else{
+        date = new Date();
+    }
+
+    return date != "Invalid Date" ? {unix: Date.parse(date), utc: date.toUTCString()}:{error: "Invalid date"};
+}
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/frontend/index.html')
 })
 
 app.get('/api/:data?',(req,res)=>{
     let data = req.params.data;
-    let date;
-    if(data){
-        if(data.includes('-')){
-            date = new Date(data);
-            data = Date.parse(date);
-        }
-        else{
-            data = +data;
-            date = new Date(data);
-        }
-    }
-    else{
-        date = new Date();
-        data = Date.parse(date);
-    }
-    if(date == "Invalid Date"){
-        res.json({error : "Invalid Date"});
-    }
-    else{
-        res.json({unix: data, utc: date.toUTCString()});
-    }
+    const response = dateConverter(data);
+    res.json(response);
 })
 
 app.listen(port, () => {
